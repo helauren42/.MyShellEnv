1daca7ad84f959639cc68ec51d57a3a7a7cbcf60
helauren-laptop <helauren42@gmail.com>
2024-12-03 02:54:49 +0100

    testing this script to make commits

---
+++ .git_commit.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import subprocess
+
+def runCommand(command):
+	result = subprocess.run(command, shell=True, text=True, capture_output=True)
+
+	if result.returncode:
+		print(f"Error: {result.stderr}")
+		sys.exit(1)
+	else:
+		print(result.stdout)
+
+def main():
+
+	if len(sys.argv) < 2:
+		print("Usage: ./gitcom")
+		sys.exit(1)
+	
+	message = sys.argv[1]
+
+	if len(sys.argv) > 2:
+		message = " ".join(sys.argv[1:])
+
+	message = message.strip()
+	print(message)
+
+	os.system("git add .")
+	runCommand(f"git commit -m \"{message}\"")
+
+if __name__ == "__main__":
+	main()
