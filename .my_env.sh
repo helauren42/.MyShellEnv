b7a427a4a66ffa2a1d7e92c6a1639bf0b0920486
helauren-laptop <helauren42@gmail.com>
2024-12-03 03:16:10 +0100

    test

--- .my_env.sh
+++ .my_env.sh
@@ -1,4 +1,18 @@
+echo "RUNNING THIS SCRIPT"
+
+echo $PATH
+echo " "
+echo " "
+
+export PATH="home/helauren/.MyEnv/bin:$PATH"
+
+echo $PATH
+echo " "
+echo " "
+
 export PATH="/home/helauren/AS/android-studio/bin:$PATH"
 export PATH="/home/helauren/AS/flutter/bin:$PATH"
 
-
+echo $PATH
+echo " "
+echo " "
