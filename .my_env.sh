5e8a48684e372f85987f2ab79a3f21109b01595b
helauren-laptop <helauren42@gmail.com>
2025-01-08 18:56:22 +0100

    added python3 alias

--- .my_env.sh
+++ .my_env.sh
@@ -3,7 +3,9 @@ export PATH="/home/helauren/.MyEnv/bin:$PATH"
 export PATH="/home/helauren/AS/android-studio/bin:$PATH"
 export PATH="/home/helauren/AS/flutter/bin:$PATH"
 
+export PATH="$PATH:~/Qt/Tools/QtCreator/bin"
+
 alias resetDns='echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf'
 
-alias Path="$PATH:~/Qt/Tools/QtCreator/bin"
+alias p="python3"
 
