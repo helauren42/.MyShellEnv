493b416ae80a150aadc301dd5280bbe3cef77016
helauren <helauren42@gmail.com>
2025-01-12 14:49:48 +0100

    added back gitcom alias

--- update.sh
+++ update.sh
@@ -5,9 +5,12 @@ export PATH="/home/helauren/AS/flutter/bin:$PATH"
 
 export PATH="$PATH:~/Qt/Tools/QtCreator/bin"
 
-alias resetDns='echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf'
+alias reset_dns='echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf'
 
 alias p="python3"
 
 alias vpn_torrent="sudo cyberghostvpn --connect --country-code BE --torrent"
 
+alias gitcom="~/.MyShellEnv/bin/gitcom"
+
+alias set_gnome_icons="gsettings set org.gnome.desktop.interface icon-theme"
