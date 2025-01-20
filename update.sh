d69b6cc2c438eb0f2dccf7cc6d6b1c71a3909a24
helauren <helauren42@gmail.com>
2025-01-20 05:06:30 +0100

    more aliases and convenient functions

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+ERROR_REQUIRES_ARG="Error: requires an argument"
+
 # PATHS
 export PATH="/home/helauren/.MyEnv/bin:$PATH"
 export PATH="$PATH:~/Qt/Tools/QtCreator/bin"
@@ -8,17 +10,16 @@ touchx() {
 	touch "$1" && chmod u+x "$1"
 }
 
-# github
-
-alias ga="git add"
-alias gc="git commit -m"
-alias gp="git push"
-
-gt() {
-	ga . && gc "$1" && gp
+cdup() {
+	parent=""
+	for ((i=1; i<=$1; i++));
+	do
+		parent+="../"
+	done;
+	cd "$parent"
 }
 
-# Aliases
+# alias
 
 alias reset_dns='echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf'
 
@@ -27,3 +28,39 @@ alias p="python3"
 alias vpn_torrent="sudo cyberghostvpn --connect --country-code BE --torrent"
 
 alias set_gnome_icons="gsettings set org.gnome.desktop.interface icon-theme"
+
+# dnf
+
+install() {
+	sudo dnf install $1 -y
+}
+
+alias remove="sudo dnf remove"
+alias upgrade="sudo dnf upgrade -y"
+
+# github
+
+alias gl="git log"
+alias gd="git diff"
+alias gpull="git pull"
+alias gs="git status"
+
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+
+gt() {
+	if [ -z $1 ]; then
+		echo $ERROR_REQUIRES_ARG
+	else
+		ga . && gc "$1" && gp
+	fi
+}
+
+grmc() {
+	if [ -z $1 ]; then
+		echo $ERROR_REQUIRES_ARG
+	else
+		git rm --cached $1
+	fi
+}
