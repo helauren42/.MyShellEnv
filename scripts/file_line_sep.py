fab10e171a45fa23ca38cdb805e6c42cb33104fa
helauren <helauren42@gmail.com>
2025-02-20 09:01:28 +0100

    added file line separator script

---
+++ scripts/file_line_sep.py
@@ -0,0 +1,35 @@
+import argparse
+from typing import Optional
+import sys
+
+parser = argparse.ArgumentParser(prog="file line separator", description="create a line separator for the given line in file")
+
+parser.add_argument("file", type=str, help="The file in which to add a line separator")
+parser.add_argument("line", type=int, help="The line number in which to add the line separator")
+parser.add_argument("-c", "--comment", nargs='?', type=str, default=None, help="The comment in the middle of the line (optional)")
+
+args = parser.parse_args()
+
+def readFile() -> list[str]:
+    with open(args.file, "r") as file:
+        red = file.readlines()
+    return red
+
+def addLine(lines: list[str]):
+    with open(args.file, "w") as file:
+        for i, line in enumerate(lines):
+            if i == args.line -1 and args.comment == None:
+                file.write(120 * '-' + '\n')
+            elif i == args.line -1:
+                num = int((120 - (len(args.comment) + 2)) // 2)
+                file.write(num * '-' + f" {args.comment} " + num * '-' + '\n')
+            file.write(line)
+
+def main():
+    lines = readFile()
+    if args.line > len(lines):
+        print(f"The file has {len(lines)} lines can't write to line {args.line}")
+        sys.exit(1)
+    addLine(lines)
+
+main()
