658704a29604dca0d6b578a76890f01424ab1f2a
helauren42@archcraft <helauren42@gmail.com>
2025-03-06 23:25:57 +0100

    split dnf.sh and minor other tweaks

--- update.sh
+++ update.sh
@@ -4,12 +4,10 @@ ERROR_REQUIRES_ARG="Error: requires an argument"
 
 # WELCOME
 
-date
-
 # PATHS
 export PATH="$PATH:~/Qt/Tools/QtCreator/bin"
 
-# docker 
+# docker
 
 alias dock_prune="docker system prune -a"
 alias dock_stop="docker stop $(docker ps -q)"
@@ -86,22 +84,8 @@ cdup() {
 
 alias rm="gio trash"
 
-alias reset_dns='echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf'
-
 alias p="python3"
-
-alias set_gnome_icons="gsettings set org.gnome.desktop.interface icon-theme"
 
 alias Obsidian="nohup ~/.local/bin/Obsidian-1.8.7.AppImage &"
 alias Joplin="nohup ~/.local/bin/Joplin-3.2.12.AppImage &"
-
-# dnf
-
-install() {
-	sudo dnf install $1 -y
-}
-
-alias remove="sudo dnf remove"
-alias upgrade="sudo dnf upgrade -y"
-
 
