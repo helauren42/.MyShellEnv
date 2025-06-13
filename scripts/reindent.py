be32d7c3b36510a765b4b247c7b18ac688bda545
helauren42 <helauren42@gmail.com>
2025-06-14 01:50:56 +0200

    reindent can handle multiple files

--- scripts/reindent.py
+++ scripts/reindent.py
@@ -1,9 +1,8 @@
 import sys
 
 def help(file=sys.stdout):
-    print("Ex: retab file_name old_format size new_format size", file=file)
+    print("Ex: reindent old_format size new_format size files..", file=file)
     print('format must be described as "spaces" or "tabs"', file=file)
-    print('indeed if you want tabs as format it doesn\'t make sense to ask for a numeric string as size but just put one for goodness sake')
     sys.exit(0)
 
 def error():
@@ -12,61 +11,66 @@ def error():
 
 class checkValid:
     @staticmethod
-    def isFormat(word:str):
-        if(word == "spaces" or word == "tabs"):
-           return True
-        return False
-    @staticmethod
     def validArgs():
         if len(sys.argv) == 1 or (len(sys.argv) == 2 and (sys.argv[1] == "--help" or sys.argv[1] == "-h")):
             help()
-        if len(sys.argv) != 6:
+        if len(sys.argv) < 6:
             error()
-        if not (sys.argv[3].isdigit() and sys.argv[5].isdigit() and int(sys.argv[3]) > 0 and int(sys.argv[5]) > 0):
+        if not (sys.argv[2].isdigit() and sys.argv[4].isdigit() and int(sys.argv[2]) > 0 and int(sys.argv[4]) > 0):
+            error()
+        if sys.argv[1] not in ["spaces", "tabs"] or sys.argv[3] not in ["spaces", "tabs"]:
             error()
 
-checkValid.validArgs();
+checkValid.validArgs()
 
 class replaceIndent:
-    old_format = sys.argv[2]
-    new_format = sys.argv[4]
-    target = int(sys.argv[3])
-    new_size = int(sys.argv[5])
-    indent = " " * target if old_format == "spaces" else "\t"
-    new_indent = "\t" if new_format == "tabs" else " " * new_size
+    old_format = sys.argv[1]
+    new_format = sys.argv[3]
+    old_size = int(sys.argv[2])
+    new_size = int(sys.argv[4])
 
     @staticmethod
     def replace(line: str):
-        blocks = 0
+        # Calculate total visual columns from leading whitespace
+        total_columns = 0
         i = 0
+        
         if replaceIndent.old_format == "tabs":
-            while i < len(line) and line[i] == "\t":
-                blocks += 1
+            # Count tabs and multiply by tab width (old_size)
+            tab_count = 0
+            while i < len(line) and line[i] == '\t':
+                tab_count += 1
                 i += 1
-        else:
-            current_count = 0
-            while i < len(line) and line[i] == " ":
-                current_count += 1
-                if current_count == replaceIndent.target:
-                    blocks += 1
-                    current_count = 0
+            total_columns = tab_count * replaceIndent.old_size
+        else:  # old_format == "spaces"
+            # Count raw spaces (ignore old_size for column count)
+            space_count = 0
+            while i < len(line) and line[i] == ' ':
+                space_count += 1
                 i += 1
-        if blocks == 0:
-            return line
-        line = line[i:]
-        line = replaceIndent.new_indent * blocks + line
-        return line
+            total_columns = space_count
 
-def doYourThing():
-    with open(sys.argv[1], "r") as file:
+        # Generate new indentation
+        if replaceIndent.new_format == "tabs":
+            tabs = total_columns // replaceIndent.new_size
+            spaces = total_columns % replaceIndent.new_size
+            new_indent = "\t" * tabs + " " * spaces
+        else:  # new_format == "spaces"
+            new_indent = " " * total_columns
+
+        return new_indent + line[i:]
+
+def doYourThing(filename):
+    with open(filename, "r") as file:
         lines = file.readlines()
-        with open(sys.argv[1], 'w') as writeFile:
-            writeFile.write("")
-            for line in lines:
-                line = replaceIndent.replace(line=line)
-                writeFile.write(line)
+    with open(filename, 'w') as writeFile:
+        for line in lines:
+            processed_line = replaceIndent.replace(line)
+            writeFile.write(processed_line)
 
 def main():
-    doYourThing()
+    for i in range(5, len(sys.argv)):
+        doYourThing(sys.argv[i])
 
-main()
+if __name__ == "__main__":
+    main()
