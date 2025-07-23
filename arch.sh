581788897f7adfb338b2003612d0e54fb98ac3ec
helauren42 <helauren42@gmail.com>
2025-07-23 10:54:58 +0200

    remove distro specific scripts

--- arch.sh
+++
@@ -1,4 +0,0 @@
-alias update="sudo pacman -Syu"
-alias install="sudo pacman -S"
-alias remove="sudo pacman -Rns"
-alias search="pacman -Ss"
--- dnf.sh
+++
@@ -1,7 +0,0 @@
- install() {
-     sudo dnf install $1 -y
-}
-
-alias remove="sudo dnf remove"
-alias upgrade="sudo dnf upgrade -y"
-
